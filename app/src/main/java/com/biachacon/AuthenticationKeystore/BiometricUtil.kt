fcf296d37bdec53e74e70ef14bb368a2a08d562e
biachacon <anabheatrizchacon@outlook.com>
2019-10-31 11:40:19 -0300

    .

---
+++ app/src/main/java/com/biachacon/AuthenticationKeystore/BiometricUtil.kt
@@ -0,0 +1,41 @@
+package com.biachacon.AuthenticationKeystore
+
+import android.Manifest
+import android.content.Context
+import android.os.Build
+import androidx.core.hardware.fingerprint.FingerprintManagerCompat
+import android.content.pm.PackageManager
+import android.Manifest.permission
+import android.Manifest.permission.USE_FINGERPRINT
+import androidx.core.app.ActivityCompat
+
+
+
+
+
+class BiometricUtil {
+
+    fun isBiometricPromptEnabled():Boolean{
+        return (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)
+    }
+
+    fun isSdkVersionSupported():Boolean{
+        return (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+    }
+
+    fun isHardwareSupported(context: Context): Boolean {
+        val fingerprintManager = FingerprintManagerCompat.from(context)
+        return fingerprintManager.isHardwareDetected
+    }
+
+    fun isFingerprintAvailable(context: Context): Boolean {
+        val fingerprintManager = FingerprintManagerCompat.from(context)
+        return fingerprintManager.hasEnrolledFingerprints()
+    }
+
+    fun isPermissionGranted(context: Context): Boolean {
+        return ActivityCompat.checkSelfPermission(context, Manifest.permission.USE_FINGERPRINT) ==
+                PackageManager.PERMISSION_GRANTED
+    }
+
+}
